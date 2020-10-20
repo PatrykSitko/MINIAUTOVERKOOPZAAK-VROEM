1c8eb248113c15a51dc27e665f92fe24ee59e036
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-10-20 11:48:47 +0200

    Main for testing purposes

--- src/main/java/be/intecbrussel/patryksitko/App.java
+++ src/main/java/be/intecbrussel/patryksitko/App.java
@@ -1,13 +1,17 @@
 package be.intecbrussel.patryksitko;
 
+import java.util.Optional;
+
+import be.intecbrussel.patryksitko.dataAccessObject.EmployeeDao;
+import be.intecbrussel.patryksitko.dataAccessObject.EmployeeDaoImpl;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        EmployeeDao cdao = new EmployeeDaoImpl();
+        System.out.println(cdao.getAll(Optional.empty(), Optional.empty()));
     }
 }
